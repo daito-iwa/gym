f45a0f6dc88266642b496e9bbfa0a28577753512
岩崎大翔 <iwasakihiroto@iwasakihirotonoMacBook-Pro.local>
2025-08-06 00:53:46 +0900

    Disable persistent skill cache loading for debugging

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "eb13308e9e51ffc789c7ba55318294dd",
+const RESOURCES = {"flutter_bootstrap.js": "3a0a219c9d8bd77de3330cfb77123d34",
 "version.json": "b5d52a3eae84d49b4b5b74717cd65ed8",
 "index.html": "417442171b7965a0d729e87f593ef352",
 "/": "417442171b7965a0d729e87f593ef352",
-"main.dart.js": "f258953672e4af9615fb10186c24e53b",
+"main.dart.js": "8fe1395323794304448a95eb0875cb03",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
